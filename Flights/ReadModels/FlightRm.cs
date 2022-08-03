9b0a2dc50a8507485c52ef0634186c0eda15dc89
Filip Kasprzyk <kasprzyk.filip.99@gmail.com>
2022-08-03 10:05:15 +0200

    Add Swagger and Open API. Create records for FlightController.

---
+++ Flights/ReadModels/FlightRm.cs
@@ -0,0 +1,12 @@
+﻿namespace Flights.ReadModels
+{
+	public record FlightRm
+	(
+		Guid Id,
+		string Airline,
+		string Place,
+		TimePlaceRm Departure,
+		TimePlaceRm Arrival,
+		int RemainingNumberOfSeats
+	);
+}
